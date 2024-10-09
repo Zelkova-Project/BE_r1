25dd55f011b1d4c253e758e27e150e42ba19a053
tomhoon <gnsdl9108@naver.com>
2024-10-09 23:37:09 +0900

    fix: kakao 정보 확인용 log 추가

--- src/main/java/com/zelkova/zelkova/controller/SocialController.java
+++ src/main/java/com/zelkova/zelkova/controller/SocialController.java
@@ -13,19 +13,21 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@Log4j2
 public class SocialController {
 
  private final MemberService memberService;
 
  @GetMapping("/api/member/kakao")
  public Map<String, Object> getMemberFromKakao(String accessToken) {
+  log.info("kakao .... ");
   MemberDTO memberDTO = memberService.getKakaoMember(accessToken);
 
   Map<String, Object> claims = memberDTO.getClaims();
 
   claims.put("accessToken", JWTUtil.generateToken(claims, 10));
   claims.put("refreshToken", JWTUtil.generateToken(claims, 60 * 24));
-
+  log.info("kakao claims.... " + claims);
   return claims;
  }
 }
