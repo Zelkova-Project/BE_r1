b650dd5767ed59416f5fc41933cde16ff30bf86d
TomHoon <99778185+TomHoon@users.noreply.github.com>
2024-10-18 16:03:13 +0900

    feat: img 확장자 webp 처리로직 추가

--- src/main/java/com/zelkova/zelkova/controller/ImageController.java
+++ src/main/java/com/zelkova/zelkova/controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.zelkova.zelkova.controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.core.io.Resource;
@@ -37,6 +39,15 @@ public class ImageController {
 
     return Map.of("imageNames", uploadFileNames);
   }
+
+    // 이미지만 등록할 때
+    @PostMapping("/webp/")
+    public Map<String, String> registerWebp(@RequestParam("file") MultipartFile file) throws IOException {
+  
+      String webpName = fileUtil.saveFile(file);
+  
+      return Map.of("uploadFileName", webpName);
+    }
 
   @GetMapping("/view/{filename}")
   public ResponseEntity<Resource> viewFile(@PathVariable(name = "filename") String filename) {
