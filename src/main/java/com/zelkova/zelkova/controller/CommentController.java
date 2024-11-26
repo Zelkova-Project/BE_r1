27b2975c81f3f5f26f277e5f833fd5d4452576d9
tomhoon <gnsdl9108@naver.com>
2024-11-26 11:13:09 +0900

    refactor: api common response 추가

--- src/main/java/com/zelkova/zelkova/controller/CommentController.java
+++ src/main/java/com/zelkova/zelkova/controller/CommentController.java
@@ -4,7 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zelkova.zelkova.dto.CommentDTO;
+import com.zelkova.zelkova.dto.CommonResponse;
 import com.zelkova.zelkova.service.CommentService;
+import com.zelkova.zelkova.util.ApiResponseUtil;
 import com.zelkova.zelkova.util.JWTUtil;
 
 import lombok.RequiredArgsConstructor;
@@ -13,7 +15,6 @@ import lombok.extern.log4j.Log4j2;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,19 +33,19 @@ public class CommentController {
     private final CommentService commentService;
 
     @GetMapping("/{bno}")
-    public List<CommentDTO> get(@PathVariable(name = "bno") Long bno) {
+    public ResponseEntity<CommonResponse<Object>> get(@PathVariable(name = "bno") Long bno) {
         List<CommentDTO> list = commentService.get(bno);
-        return list;
+        return ApiResponseUtil.success(list);
     }
 
     @PostMapping("/")
-    public Long register(@RequestBody CommentDTO commentDTO) {
+    public ResponseEntity<CommonResponse<Object>> register(@RequestBody CommentDTO commentDTO) {
         Long cno = commentService.register(commentDTO);
-        return cno;
+        return ApiResponseUtil.success(cno);
     }
 
     @PutMapping("/like/{id}")
-    public Map<String, String> addCommentLike(@RequestHeader("Authorization") String authorization, @PathVariable Long id) {
+    public ResponseEntity<CommonResponse<Object>> addCommentLike(@RequestHeader("Authorization") String authorization, @PathVariable Long id) {
         // [START] User Identifying 
         String authHeaderStr = authorization;
 
@@ -56,23 +57,26 @@ public class CommentController {
 
         // comment id를 받아서 like를 추가함
         commentService.addCommentLike(id, email);
-        return Map.of("Result", "Success");
+        Map<String, String> result =  Map.of("Result", "Success");
+        return ApiResponseUtil.success(result);
     }
 
     @GetMapping("/likedUserList/{bid}")
-    public List<List<String>> getLikedUserList(@PathVariable(name = "bid") Long bid) {
+    public ResponseEntity<CommonResponse<Object>> getLikedUserList(@PathVariable(name = "bid") Long bid) {
         // board id를 넣어 조회하면
         // board에 존재하는 댓글의 좋아요를 누른 이메일만 뽑아서 리턴
 
-        return commentService.getLikedUserList(bid);
+        List<List<String>> result =  commentService.getLikedUserList(bid);
+        return ApiResponseUtil.success(result);
     }
 
     @DeleteMapping("/{bno}")
-    public ResponseEntity<Map<String, String>> deleteComment(@PathVariable Long bno) {
+    public ResponseEntity<CommonResponse<Object>> deleteComment(@PathVariable Long bno) {
         Map<String, String> result = commentService.deleteComment(bno);
-        return ResponseEntity.status(HttpStatus.OK).body(result);
+        return ApiResponseUtil.success(result);
     }
     
 }
+
 
 
--- src/main/java/com/zelkova/zelkova/controller/ProfileController.java
+++ src/main/java/com/zelkova/zelkova/controller/ProfileController.java
@@ -3,12 +3,15 @@ package com.zelkova.zelkova.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zelkova.zelkova.dto.CommonResponse;
 import com.zelkova.zelkova.service.BoardSerivce;
+import com.zelkova.zelkova.util.ApiResponseUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,7 +23,7 @@ public class ProfileController {
     private final BoardSerivce boardSerivce;
 
     @GetMapping("/userinfo/{email}")
-    public Map<Object, Object> getUserInfo(@PathVariable(name = "email") String email) {
+    public ResponseEntity<CommonResponse<Object>> getUserInfo(@PathVariable(name = "email") String email) {
 
         int 게시글개수 = boardSerivce.getBoardCountByEmail(email);
 
@@ -31,7 +34,8 @@ public class ProfileController {
         result.put("아는친구", 0);
         result.put("친한친구", 0);
 
-        return result;
+        return ApiResponseUtil.success(result);
     }
 }
+
 
