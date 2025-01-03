c49f5a65346b63f957c61d72e90b24e2346a5d2b
tomhoon <gnsdl9108@naver.com>
2025-01-03 17:25:26 +0900

    feat: sms 기본 전송기능 추가

---
+++ src/main/java/com/zelkova/zelkova/dto/message/MessageDTO.java
@@ -0,0 +1,12 @@
+package com.zelkova.zelkova.dto.message;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MessageDTO {
+    private String receiveNumber;
+    private String message;
+}
+
